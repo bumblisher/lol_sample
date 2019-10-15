1ea5112351de6ad3a010d93608c3e495b5bce774
bumblisher <kjb6558@naver.com>
2019-10-15 20:45:55 +0900

    메인 페이지 라인별

--- pub/resource/js/common.js
+++ pub/resource/js/common.js
@@ -1,8 +1,16 @@
 $(window).ready(function(){
+     // 메인 그래프 호출
     draw(80, '#piechart1', '#2f2fd3');
     draw(56, '#piechart2', '#2f2fd3');
+
+
+    // 슬라이드토글
+    $('.list_slide .btn_slide').on('click',function(){
+		$(this).toggleClass('on').siblings().stop().slideToggle();
+    });
  });
  
+ // 메인 그래프
  function draw(max, classname, colorname){
     var i=1;
      var func1 = setInterval(function(){
@@ -14,6 +22,7 @@ $(window).ready(function(){
        }
      },10);
  }
+  // 메인 그래프 숫자
  function color1(i, classname,colorname){
     $(classname).css({
          "background":"conic-gradient("+colorname+" 0% "+i+"%, transparent "+i+"% 100%)"
@@ -21,7 +30,7 @@ $(window).ready(function(){
     $(classname).siblings().html(++i + "%");
  }
  
-
+  // 팝업 띄우기
 openPopup = function(id, url, options){
 	$('html').addClass("openpop");
 	$('body').append("<div class='dim'></div>");
